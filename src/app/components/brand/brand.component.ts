2caff11d13ab1fb7e83f5515855427798aec5384
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-16 09:19:13 +0300

    Brand component created.

---
+++ src/app/components/brand/brand.component.ts
@@ -0,0 +1,26 @@
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from './../../services/brand.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand',
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.scss'],
+})
+export class BrandComponent implements OnInit {
+  brands: Brand[] = [];
+  dataLoaded: boolean = false;
+
+  constructor(private brandService: BrandService) {}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.brandService.getAll().subscribe((response) => {
+      this.brands = response.data;
+      this.dataLoaded = true;
+    });
+  }
+}
