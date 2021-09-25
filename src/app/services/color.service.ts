db6f026641e3ee68eb68c3a39a90d3048b5424ae
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:35:53 +0300

    Modified according to current selection option.

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ListResponseModel } from './../models/listResponseModel';
 import { Observable } from 'rxjs';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,12 @@ export class ColorService {
   getAll(): Observable<ListResponseModel<Color>> {
     return this.httpClient.get<ListResponseModel<Color>>(
       `${this.apiURL}/getall`
+    );
+  }
+
+  getById(id: number): Observable<SingleResponseModel<Color>> {
+    return this.httpClient.get<SingleResponseModel<Color>>(
+      `${this.apiURL}/getbyid?id=${id}`
     );
   }
 }
