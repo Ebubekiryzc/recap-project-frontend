dc3359d46390def68018f46e816f57ba2bb19a60
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:33:46 +0300

    Modified according to new needs.

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Observable } from 'rxjs';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -16,9 +17,48 @@ export class CarService {
     return this.httpClient.get<ListResponseModel<Car>>(`${this.apiURL}/getall`);
   }
 
+  getById(id: number): Observable<SingleResponseModel<Car>> {
+    return this.httpClient.get<SingleResponseModel<Car>>(
+      `${this.apiURL}/getbyid?id=${id}`
+    );
+  }
+
   getAllWithDetails(): Observable<ListResponseModel<CarDetail>> {
     return this.httpClient.get<ListResponseModel<CarDetail>>(
       `${this.apiURL}/getcarswithdetail`
+    );
+  }
+
+  getCarWithDetailsById(
+    id: number
+  ): Observable<SingleResponseModel<CarDetail>> {
+    return this.httpClient.get<SingleResponseModel<CarDetail>>(
+      `${this.apiURL}/getcarwithdetailbyid?id=${id}`
+    );
+  }
+
+  getAllCarsWithDetailsByBrandId(
+    brandId: number
+  ): Observable<ListResponseModel<CarDetail>> {
+    return this.httpClient.get<ListResponseModel<CarDetail>>(
+      `${this.apiURL}/getcarswithdetailbybrandid?brandId=${brandId}`
+    );
+  }
+
+  getAllCarsWithDetailsByColorId(
+    colorId: number
+  ): Observable<ListResponseModel<CarDetail>> {
+    return this.httpClient.get<ListResponseModel<CarDetail>>(
+      `${this.apiURL}/getcarswithdetailbycolorid?colorId=${colorId}`
+    );
+  }
+
+  getAllCarsWithDetailsByBrandIdAndColorId(
+    brandId: number,
+    colorId: number
+  ): Observable<ListResponseModel<CarDetail>> {
+    return this.httpClient.get<ListResponseModel<CarDetail>>(
+      `${this.apiURL}/getcarswithdetailbybrandidandcolorid?brandId=${brandId}&colorId=${colorId}`
     );
   }
 }
