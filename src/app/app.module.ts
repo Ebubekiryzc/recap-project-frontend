2429411b9e6c3948ef97dbd7ed0dcfb908b1b3eb
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:21:02 +0300

    Refactored and added new parts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { ColorComponent } from './components/color/color.component';
 import { IndividualCustomerComponent } from './components/individual-customer/individual-customer.component';
-import { CarComponent } from './components/car/car.component';
 import { RentalComponent } from './components/rental/rental.component';
+import { SanitizerService } from './services/sanitizer.service';
 
 @NgModule({
-  declarations: [AppComponent, BrandComponent, ColorComponent, IndividualCustomerComponent, CarComponent, RentalComponent],
+  declarations: [
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    IndividualCustomerComponent,
+    CarComponent,
+    RentalComponent,
+    CarDetailsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [SanitizerService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
