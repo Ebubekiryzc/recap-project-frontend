20712f74db47a7672205554c88f2de738f229fa9
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:32:29 +0300

    New service method created for current selection.

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ListResponseModel } from './../models/listResponseModel';
 import { Observable } from 'rxjs';
+import { SingleResponseModel } from './../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,12 @@ export class BrandService {
   getAll(): Observable<ListResponseModel<Brand>> {
     return this.httpClient.get<ListResponseModel<Brand>>(
       `${this.apiURL}/getall`
+    );
+  }
+
+  getById(id: number): Observable<SingleResponseModel<Brand>> {
+    return this.httpClient.get<SingleResponseModel<Brand>>(
+      `${this.apiURL}/getbyid?id=${id}`
     );
   }
 }
