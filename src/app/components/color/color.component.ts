c9b40f23d88467cf5d60817240a11c7919c30f15
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:25:30 +0300

    Added current color selection.

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { Color } from 'src/app/models/color';
 import { ColorService } from 'src/app/services/color.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-color',
@@ -9,14 +9,33 @@ import { ColorService } from 'src/app/services/color.service';
 })
 export class ColorComponent implements OnInit {
   colors: Color[];
+  currentColor?: Color;
   dataLoaded: boolean = false;
+  allColors: string = 'All Colors';
+
   constructor(private colorService: ColorService) {}
 
   ngOnInit(): void {
     this.getAll();
   }
 
-  getAll() {
+  setCurrentColor(color?: Color): void {
+    this.currentColor = color;
+  }
+
+  getCurrentColorClass(color?: Color): string {
+    return this.currentColor === color
+      ? 'list-group-item active text-center'
+      : 'list-group-item text-center';
+  }
+
+  getAllColorClass(): string {
+    return !this.currentColor
+      ? 'list-group-item list-group-item-action bg-secondary text-light text-center'
+      : 'list-group-item list-group-item-action text-center';
+  }
+
+  getAll(): void {
     this.colorService.getAll().subscribe((response) => {
       this.colors = response.data;
       this.dataLoaded = true;
