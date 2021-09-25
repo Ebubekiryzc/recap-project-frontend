593e05eaee766fb42bea6140b6d225e510b72819
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-25 17:36:16 +0300

    Get by id methods created.

--- src/app/services/individual-customer.service.ts
+++ src/app/services/individual-customer.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { IndividualCustomer } from '../models/individualCustomer';
+import { Injectable } from '@angular/core';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Observable } from 'rxjs';
+import { SingleResponseModel } from './../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,12 @@ export class IndividualCustomerService {
   getAll(): Observable<ListResponseModel<IndividualCustomer>> {
     return this.httpClient.get<ListResponseModel<IndividualCustomer>>(
       `${this.apiURL}/getall`
+    );
+  }
+
+  getById(id: number): Observable<SingleResponseModel<IndividualCustomer>> {
+    return this.httpClient.get<SingleResponseModel<IndividualCustomer>>(
+      `${this.apiURL}/getbyid?id=${id}`
     );
   }
 }
--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,3 +1,4 @@
+import { SingleResponseModel } from './../models/singleResponseModel';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ListResponseModel } from '../models/listResponseModel';
@@ -17,5 +18,13 @@ export class RentalService {
     return this.httpClient.get<
       ListResponseModel<RentalDetailWithIndividualCustomers>
     >(`${this.apiURL}/getallwithindividualcustomerdetails`);
+  }
+
+  getRentalWithIndividualCustomerDetailsById(
+    id: number
+  ): Observable<SingleResponseModel<RentalDetailWithIndividualCustomers>> {
+    return this.httpClient.get<
+      SingleResponseModel<RentalDetailWithIndividualCustomers>
+    >(`${this.apiURL}/getwithindividualcustomerdetailsbyrentalid?id=${id}`);
   }
 }
