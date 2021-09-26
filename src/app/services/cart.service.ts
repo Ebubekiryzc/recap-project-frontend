a2b15954bd43ac99a71ea911e75c7fdaa4adb31f
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-26 20:21:43 +0300

    Cart service created.

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,41 @@
+import { CarDetail } from 'src/app/models/carDetail';
+import { Injectable } from '@angular/core';
+import { Car } from '../models/car';
+import { CartItem } from '../models/cartItem';
+import { CartItems } from '../models/cartItems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  addToCart(car: CarDetail) {
+    let item = CartItems.find((c) => c.car.carId === car.carId);
+    if (item) {
+      item.quantity += 1;
+    } else {
+      let cartItem = new CartItem();
+      cartItem.car = car;
+      cartItem.quantity = 1;
+      CartItems.push(cartItem);
+    }
+  }
+
+  removeFromCart(car: CarDetail) {
+    let item: CartItem = CartItems.find(
+      (c) => c.car.carId === car.carId
+    );
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    } else {
+      CartItems.splice(CartItems.indexOf(item), 1);
+    }
+  }
+
+  list(): CartItem[] {
+    return CartItems;
+  }
+  
+}
