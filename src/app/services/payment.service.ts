3e255b65f10d92ae7328e786dbde69ec78cbb199
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2021-09-28 15:44:17 +0300

    New method added.

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaymentQuery } from './../models/paymentQuery';
+import { ResponseModel } from './../models/responseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +10,11 @@ import { Injectable } from '@angular/core';
 export class PaymentService {
   private apiURL: string = 'https://localhost:44305/api/payments';
   constructor(private httpClient: HttpClient) {}
+
+  getPayment(paymentQuery: PaymentQuery): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(
+      `${this.apiURL}/getpayment`,
+      paymentQuery
+    );
+  }
 }
